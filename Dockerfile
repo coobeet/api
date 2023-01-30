9e71dd2f3303cd5f972e6f4452f01713576f8a4d
coobeet <coobeet@gmail.com>
2023-01-31 01:58:26 +0800

    feat: add ci

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.19.5-alpine AS builder
+
+WORKDIR /workspace
+
+RUN apk add --update --no-cache git && rm -rf /var/cache/apk/*
+COPY go.mod go.sum /workspace/
+RUN go mod download
+COPY main.go /workspace/
+RUN go build -o api .
+
+FROM alpine
+RUN apk add --update --no-cache ca-certificates tzdata && rm -rf /var/cache/apk/*
+COPY --from=builder /workspace/api /usr/local/bin/api
+CMD [ "/usr/local/bin/api" ]
--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"buf.build/gen/go/coobeet/coobeet/bufbuild/connect-go/coobeet/v1/coobeetv1connect"
 	coobeetv1 "buf.build/gen/go/coobeet/coobeet/protocolbuffers/go/coobeet/v1"
@@ -12,15 +18,15 @@ import (
 	"golang.org/x/net/http2/h2c"
 )
 
-type echoServer struct {
+type EchoServer struct {
 	coobeetv1connect.UnimplementedEchoServiceHandler
 }
 
-func NewEchoServer() *echoServer {
-	return &echoServer{}
+func NewEchoServer() *EchoServer {
+	return &EchoServer{}
 }
 
-func (s *echoServer) Echo(
+func (s *EchoServer) Echo(
 	ctx context.Context,
 	req *connect.Request[coobeetv1.EchoRequest],
 ) (*connect.Response[coobeetv1.EchoResponse], error) {
@@ -29,10 +35,58 @@ func (s *echoServer) Echo(
 	}), nil
 }
 
+func createChannel() (chan os.Signal, func()) {
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	return stopCh, func() {
+		close(stopCh)
+	}
+}
+
+func start(server *http.Server) {
+	log.Println("application started")
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(err)
+	} else {
+		log.Println("application stopped gracefully")
+	}
+}
+
+func shutdown(ctx context.Context, server *http.Server) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
+	} else {
+		log.Println("application shutdowned")
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle(coobeetv1connect.NewEchoServiceHandler(NewEchoServer()))
 	handler := cors.AllowAll().Handler(mux)
 	handler = h2c.NewHandler(handler, &http2.Server{})
-	http.ListenAndServe("localhost:8080", handler)
+
+	addr := "localhost:8080"
+	if port := os.Getenv("PORT"); port != "" {
+		addr = ":" + port
+	}
+	s := &http.Server{
+		Addr:              addr,
+		Handler:           handler,
+		ReadHeaderTimeout: time.Second,
+		ReadTimeout:       5 * time.Minute,
+		WriteTimeout:      5 * time.Minute,
+		MaxHeaderBytes:    8 * 1024, // 8KiB
+	}
+	go start(s)
+
+	stopCh, closeCh := createChannel()
+	defer closeCh()
+	log.Println("notified:", <-stopCh)
+
+	shutdown(context.Background(), s)
 }
